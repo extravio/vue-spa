39cd0d44209fbf0abbb80ee67c3827931633fd76
Mikael Mrozik <mikael.mrozik@deviate.net.nz>
2018-02-26 14:58:51 +1300

    Vue router

--- vue-spa/src/js/app.js
+++ vue-spa/src/js/app.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import AppLayout from '../theme/Layout.vue';
+import router from './router';
 
 // Vue.component('app', {
 //   template:
 // })
 
 const app = new Vue({
+    router,
     ...AppLayout
 });
 
-export { app }
+export {
+    app,
+    router
+}
